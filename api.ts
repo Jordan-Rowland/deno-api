1a9fd6d62b339dc8615d1efcb9b084fd060e8310
Jordan Rowland <36084892+Jordan-Rowland@users.noreply.github.com>
2020-06-21 11:24:50 -0700

    updates

--- api.ts
+++ api.ts
@@ -1,16 +1,44 @@
 import { Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 
-const router = new Router();
+const api_router = new Router();
 
-router.get("/", (ctx) => {
+api_router.get("/", (ctx) => {
   ctx.response.body = "Hello Nasa!(From routes)";
 });
 
-router.get("/planets", (ctx) => {
-  ctx.response.body = JSON.stringify({
+api_router.get("/planets", (ctx) => {
+  ctx.response.body = {
     success: true,
     message: "Successfully fetched planets",
-  });
+  };
 });
 
-export default router;
+api_router.get("/launches", (ctx) => {
+  ctx.response.body = {
+    success: true,
+    message: "Successfully fetched launches",
+  };
+});
+
+api_router.get("/launches/:id", (ctx) => {
+  ctx.response.body = {
+    success: true,
+    message: `Successfully fetched launch #${ctx.params.id}`,
+  };
+});
+
+api_router.post("/postendpoint", async (ctx) => {
+  const req = await ctx.request.body();
+  const body = await req.value;
+  console.log(body);
+  if (body.message.includes("word")) {
+    console.log("got here");
+    ctx.response.body = {
+      success: true,
+      message: `Successfully posted body`,
+      body: body,
+    };
+  }
+});
+
+export default api_router;
--- mod.ts
+++ mod.ts
@@ -1,24 +1,59 @@
+import * as log from "https://deno.land/std/log/mod.ts";
 import { Application, send } from "https://deno.land/x/oak@v5.0.0/mod.ts";
-import api from "./api.ts";
+
+import api_router from "./api.ts";
 
 const app = new Application();
 const PORT = 8000;
 
+await log.setup({
+  handlers: {
+    console: new log.handlers.ConsoleHandler("INFO"),
+  },
+  loggers: {
+    default: {
+      level: "INFO",
+      handlers: ["console"],
+    },
+  },
+});
+
+app.addEventListener("error", (event) => {
+  log.error(event.error);
+});
+
 app.use(async (ctx, next) => {
-  await next();
+  try {
+    await next();
+  } catch (err) {
+    ctx.response.body = JSON.stringify({
+      success: false,
+      message: "encountered an error",
+      error: err,
+    });
+    // throw err;
+  }
+});
+
+app.use(async (ctx, next) => {
+  log.info("Before AWAIT NEXT 1");
+  await next(); // Rest of this function pauses until next middleware returns
+  log.info("Middleware 1");
   const time = ctx.response.headers.get("X-Response-Time");
-  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+  log.info(`${ctx.request.method} ${ctx.request.url}: ${time}`);
 });
 
 app.use(async (ctx, next) => {
   const start = Date.now();
+  log.info("Before AWAIT NEXT 2");
   await next();
+  log.info("Middleware 2");
   const delta = Date.now() - start;
   ctx.response.headers.set("X-Response-Time", `${delta}ms`);
 });
 
-app.use(api.routes());
-app.use(api.allowedMethods());
+app.use(api_router.routes());
+app.use(api_router.allowedMethods());
 
 // // Serving static files
 // app.use(async (ctx) => {
@@ -35,6 +70,7 @@ app.use(api.allowedMethods());
 // });
 
 if (import.meta.main) {
+  log.info(`Starting server on port: ${PORT}....`);
   await app.listen({
     port: PORT,
   });
